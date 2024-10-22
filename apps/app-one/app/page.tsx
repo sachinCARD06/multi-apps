5c4ee85f07cf8ae805f63bd6ade860ff108b70d5
sachinCARD06 <sachin.singh@card91.io>
2024-10-22 11:57:33 +0530

    add new page

--- apps/app-one/app/page.tsx
+++ apps/app-one/app/page.tsx
@@ -1,9 +1,12 @@
 import styles from "./page.module.css";
+import AboutPage from "./pages/AboutPage/page";
+import HomePage from "./pages/HomePage/page";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      App one
+      <HomePage />
+      <AboutPage />
     </div>
   );
 }
---
+++ apps/app-one/app/pages/AboutPage/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AboutPage = () => {
+  return (
+    <div>
+      About Page one
+    </div>
+  )
+}
+
+export default AboutPage
